e5bc403622b87ca3dc65c1211097266d385dc4fd
Igumenov Oleg <dr-glum@yandex.ru>
2021-05-23 19:50:12 +0300

    create repo

---
+++ OneButton.c
@@ -0,0 +1,297 @@
+#include "OneButton.h"
+
+void ob_init(OneButton_t *btn, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState NormState){
+    
+    btn->GPIOx = GPIOx;
+    btn->GPIO_Pin = GPIO_Pin;
+
+    btn->debounce = _debounce;
+    btn->timeout  = _timeout;
+    btn->click_timeout = _click_timeout;
+    btn->step_timeout = _step_timeout;
+
+    btn->counter = 0;
+    btn->last_counter = 0;
+    btn->last_hold_counter = 0;
+    btn->timer = 0;
+    btn->state = 0;
+    btn->flag = 0;
+
+    btn->callbackFunc = NULL;
+    btn->callbackStepFunc = NULL;
+
+}
+
+void ob_setTickMode(OneButton_t *btn, ob_tick_mode mode){
+	btn->flags.tickMode = mode;
+}
+
+void ob_tick(OneButton_t *btn){
+
+  // btn_state: 1-нажато, 0-нет
+  btn->state = !btn->norm_state ^ ((btn->GPIOx->IDR & btn->GPIO_Pin) != GPIO_PIN_RESET);
+
+  uint32_t thisMls = HAL_GetTick();
+
+  // нажатие
+  if (btn->state && !btn->flag){
+  	if (!btn->flags.btn_deb){
+  		btn->flags.btn_deb = 1;
+  		btn->timer = thisMls;
+  	} else {
+  		if (thisMls - btn->timer >= btn->debounce){
+  			btn->flag = 1;
+  			btn->flags.isPress_f = 1;
+  			btn->flags.oneClick_f = 1;
+
+  			if (!btn->flags.no_callback && btn->callbackFunc) btn->callbackFunc(bf_press);
+  		}
+  	}
+  } else {
+      btn->flags.btn_deb = 0;
+  }
+
+  // отпускание
+  if (!btn->state && btn->flag) {
+  	btn->flag = 0;
+  	if (!btn->flags.hold_flag) btn->counter++;
+  	btn->flags.hold_flag = 0;
+  	btn->flags.isRelease_f = 1;
+  	btn->timer = thisMls;
+
+  	if (btn->flags.step_flag){
+  		btn->last_counter = 0;
+  		btn->counter = 0;
+  		btn->flags.step_flag = 0;
+  	}
+
+  	if (btn->flags.oneClick_f){
+  		btn->flags.oneClick_f = 0;
+  		btn->flags.isOne_f = 1;
+  		if (!btn->flags.no_callback && btn->callbackFunc) btn->callbackFunc(bf_release|bf_single);
+  	} else {
+			if (!btn->flags.no_callback && btn->callbackFunc) btn->callbackFunc(bf_release);
+  	}
+
+	}
+
+  // кнопка удерживается
+  if (btn->flag && btn->state && (thisMls - btn->timer >= btn->timeout) && !btn->flags.hold_flag) {
+  	btn->flags.hold_flag = 1;
+  	btn->flags.isHolded_f = 1;
+  	btn->flags.step_flag = 1;
+  	btn->flags.oneClick_f = 0;
+  	btn->last_hold_counter = btn->counter;
+  	btn->timer = thisMls;
+  	if (!btn->flags.no_callback && btn->callbackFunc) btn->callbackFunc(bf_holded);
+	}
+
+  // обработка накликивания
+  if ((thisMls - btn->timer >= btn->click_timeout) && (btn->counter != 0) && !btn->state) {
+  	btn->last_counter = btn->counter;
+  	btn->counter = 0;
+  	btn->flags.counter_flag = 1;
+
+  	if (!btn->flags.no_callback && btn->callbackFunc) btn->callbackFunc(bf_click);
+
+  }
+
+ // обработка шагов и вызов callback функции
+ if (!btn->flags.no_callback && btn->callbackStepFunc && btn->flags.step_flag && (thisMls - btn->timer >= btn->step_timeout)) {
+	 btn->timer = thisMls;
+	 btn->callbackStepFunc();
+ }
+
+ // сброс накликивания
+  if(btn->flags.counter_reset) {
+  	btn->last_counter = 0;
+  	btn->flags.counter_flag = 0;
+  	btn->flags.counter_reset = 0;
+  }
+
+}
+
+void ob_resetStates(OneButton_t *btn){
+	btn->flags.isPress_f = 0;
+	btn->flags.isRelease_f = 0;
+	btn->flags.isOne_f = 0;
+	btn->flags.isHolded_f = 0;
+	btn->flags.step_flag = 0;
+	btn->flags.counter_flag = 0;
+	btn->last_hold_counter = 0;
+	btn->last_counter = 0;
+}
+
+void ob_clearFlags(OneButton_t *btn, uint8_t flags){
+
+	if (flags & bf_press)   btn->flags.isPress_f = 0;
+	if (flags & bf_release) btn->flags.isRelease_f = 0;
+	if (flags & bf_single)  btn->flags.isOne_f = 0;
+	if (flags & bf_holded)  btn->flags.isHolded_f = 0;
+
+}
+
+void ob_attach_callbackEvent(OneButton_t *btn, callbackFunction func){
+	btn->callbackFunc = func;
+}
+
+void ob_attach_callbackStep(OneButton_t *btn, callbackStepFunction func){
+	btn->callbackStepFunc = func;
+}
+
+void ob_detach_callbackEvent(OneButton_t *btn){
+	btn->callbackFunc = NULL;
+}
+
+void ob_detach_callbackStep(OneButton_t *btn){
+	btn->callbackStepFunc = NULL;
+}
+
+uint8_t ob_isPress(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+		}
+
+	if (btn->flags.isPress_f) {
+		btn->flags.isPress_f = 0;
+		return 1;
+	} else return 0;
+
+}
+
+uint8_t ob_isRelease(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+		}
+
+	if (btn->flags.isRelease_f) {
+		btn->flags.isRelease_f = 0;
+		return 1;
+	} else return 0;
+
+}
+
+uint8_t ob_isClick(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+		}
+
+	if (btn->flags.isOne_f) {
+		btn->flags.isOne_f = 0;
+		return 1;
+	} else return 0;
+
+}
+
+uint8_t ob_isHolded(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+		}
+
+	if (btn->flags.isHolded_f) {
+		btn->flags.isHolded_f = 0;
+		return 1;
+	} else return 0;
+
+}
+
+uint8_t ob_isHold(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+		uint8_t f_nocallback = btn->flags.no_callback;
+		ob_tick(btn);
+		btn->flags.no_callback = f_nocallback;
+	}
+
+	return btn->flags.step_flag;
+
+}
+
+
+uint8_t ob_isSingle(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+		uint8_t f_nocallback = btn->flags.no_callback;
+		ob_tick(btn);
+		btn->flags.no_callback = f_nocallback;
+	}
+
+	if (btn->flags.counter_flag && btn->last_counter == 1) {
+		btn->flags.counter_reset = 1;
+		return 1;
+	} else return 0;
+}
+
+uint8_t ob_isDouble(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+		uint8_t f_nocallback = btn->flags.no_callback;
+		ob_tick(btn);
+		btn->flags.no_callback = f_nocallback;
+	}
+
+	if (btn->flags.counter_flag && btn->last_counter == 2) {
+		btn->flags.counter_reset = 1;
+		return 1;
+	} else return 0;
+}
+
+uint8_t ob_hasClicks(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+		uint8_t f_nocallback = btn->flags.no_callback;
+		ob_tick(btn);
+		btn->flags.no_callback = f_nocallback;
+	}
+
+	if (btn->flags.counter_flag) {
+		btn->flags.counter_reset = 1;
+		return 1;
+	} else return 0;
+}
+
+uint8_t ob_getClicks(OneButton_t *btn){
+	btn->flags.counter_reset = 1;
+	return btn->last_counter;
+}
+
+uint8_t ob_getHoldClicks(OneButton_t *btn){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+		}
+
+	return btn->flags.hold_flag ? btn->last_hold_counter : 0;
+
+}
+
+uint8_t ob_isStep(OneButton_t *btn, uint8_t clicks){
+
+	if (btn->flags.tickMode) {
+			uint8_t f_nocallback = btn->flags.no_callback;
+			ob_tick(btn);
+			btn->flags.no_callback = f_nocallback;
+	}
+
+	uint32_t thisMls = HAL_GetTick();
+	if (btn->counter == clicks && btn->flags.step_flag && (thisMls - btn->timer >= btn->step_timeout)){
+		btn->timer = thisMls;
+		return 1;
+	} else
+		return 0;
+
+}
---
+++ OneButton.h
@@ -0,0 +1,99 @@
+#ifndef INC_ONEBUTTON_H_
+#define INC_ONEBUTTON_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+#define _debounce 60
+#define _timeout 300
+#define _click_timeout 300
+#define _step_timeout 500
+
+#define bf_press		((uint16_t)0x01)
+#define bf_release	((uint16_t)0x02)
+#define bf_single 	((uint16_t)0x04)
+#define bf_holded		((uint16_t)0x08)
+#define bf_click		((uint16_t)0x10)
+#define bf_step			((uint16_t)0x20)
+
+typedef enum {
+	OB_TICK_NORM=0,
+	OB_TICK_AUTO=1
+} ob_tick_mode;
+
+#pragma pack(push,1)
+typedef struct {		
+	uint8_t btn_deb: 1;	
+	uint8_t hold_flag: 1;
+	uint8_t counter_flag: 1;
+	uint8_t isHolded_f: 1;
+	uint8_t isRelease_f: 1;
+	uint8_t isPress_f: 1;
+	uint8_t step_flag: 1;
+	uint8_t oneClick_f: 1;
+	uint8_t isOne_f: 1;
+	uint8_t tickMode: 1;
+	uint8_t no_callback: 1;
+	uint8_t counter_reset: 1;			//Добавлен
+} button_flags;
+#pragma pack(pop)
+
+typedef void (*callbackFunction)(uint16_t flags);
+typedef void (*callbackStepFunction)(void);
+
+typedef struct 
+{
+	GPIO_TypeDef *GPIOx;
+	uint16_t GPIO_Pin;        // 
+
+	GPIO_PinState norm_state; // свободное состояние пина кнопки
+
+	button_flags flags;
+	uint16_t debounce;
+	uint16_t timeout;
+	uint16_t click_timeout;
+	uint16_t step_timeout;
+	uint8_t counter, last_counter, last_hold_counter;
+	uint32_t timer;	
+	uint8_t state;
+	uint8_t flag;
+
+	callbackFunction callbackFunc;
+	callbackStepFunction callbackStepFunc;
+
+	
+} OneButton_t;
+
+void ob_init(OneButton_t *btn, GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState NormState); // инициализация кнопки
+void ob_setTickMode(OneButton_t *btn, ob_tick_mode mode);
+void ob_tick(OneButton_t *btn);
+void ob_resetStates(OneButton_t *btn);
+void ob_clearFlags(OneButton_t *btn, uint8_t flags);
+
+void ob_attach_callbackEvent(OneButton_t *btn, callbackFunction func);
+void ob_attach_callbackStep(OneButton_t *btn, callbackStepFunction func);
+void ob_detach_callbackEvent(OneButton_t *btn);
+void ob_detach_callbackStep(OneButton_t *btn);
+
+uint8_t ob_isPress(OneButton_t *btn);
+uint8_t ob_isRelease(OneButton_t *btn);
+uint8_t ob_isClick(OneButton_t *btn);
+uint8_t ob_isHolded(OneButton_t *btn);
+uint8_t ob_isHold(OneButton_t *btn); //+
+
+uint8_t ob_isSingle(OneButton_t *btn);
+uint8_t ob_isDouble(OneButton_t *btn);
+uint8_t ob_hasClicks(OneButton_t *btn);
+uint8_t ob_getClicks(OneButton_t *btn);
+uint8_t ob_getHoldClicks(OneButton_t *btn);
+
+uint8_t ob_isStep(OneButton_t *btn, uint8_t clicks); //+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_ONEBUTTON_H_ */
